8923a9b4550e803f2fb3e49e8f9a6d0c0d0eb330
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-04-05 01:34:15 +0530

    added: login-form

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,5 +1,35 @@
 const LoginForm = () => {
-  return <div className="border h-full"></div>;
+  return (
+    <div className="border border-[#3F3D56] rounded-md h-full mx-auto w-[70%]">
+      <h1 className="text-2xl text-center mt-5 text-white">
+        Login
+      </h1>
+      <form className="text-white space-y-5 mt-5 px-5">
+        <div className="flex flex-col gap-2">
+          <label id="email">Email</label>
+          <input
+            className="p-2 px-3 bg-transparent rounded-md border-gray-400 border"
+            type="email"
+            placeholder="johndoe@gmail.com"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label id="email">Password</label>
+          <input
+            className="p-2 px-3 bg-transparent border rounded-md border-gray-400"
+            type="password"
+            placeholder="**********"
+          />
+        </div>
+        <div className="flex flex-col gap-5">
+          <button className="bg-brand p-2 rounded-md text-black">
+            Login
+          </button>
+          <button>Login As Guest</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default LoginForm;
