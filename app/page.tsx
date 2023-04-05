62b292d3cc75ccfe837a5fd90240d8f949146e09
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-04-05 16:37:44 +0530

    added: signup form

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/components/Banner";
 import LoginForm from "@/components/LoginForm";
 import Navbar from "@/components/Navbar";
+import SignupForm from "@/components/SignupForm";
 import Image from "next/image";
 
 export default function Home() {
@@ -10,7 +11,8 @@ export default function Home() {
       <div className="h-96 w-full grid grid-cols-2">
         <Banner />
         <div>
-          <LoginForm />
+          {/* <LoginForm /> */}
+          <SignupForm />
         </div>
       </div>
     </main>
---
+++ components/SignupForm.tsx
@@ -0,0 +1,103 @@
+"use client";
+import Link from "next/link";
+import { useState, useRef, useEffect } from "react";
+
+type SignupCredentials = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+const SignupForm = () => {
+  const [signupCredentials, setSignupCredentials] =
+    useState<SignupCredentials>({
+      username: "",
+      email: "",
+      password: "",
+    });
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  const inputHandler = (
+    e: React.SyntheticEvent<HTMLInputElement>
+  ) => {
+    const { name, value } = e.currentTarget;
+    setSignupCredentials((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const formHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="border border-[#3F3D56] shadow-md rounded-md pb-5 h-fit mx-auto w-[70%]">
+      <h1 className="text-2xl text-center mt-5 text-white">
+        Sign Up
+      </h1>
+      <form
+        onSubmit={formHandler}
+        className="text-white space-y-5 mt-5 px-5"
+      >
+        <div className="flex flex-col gap-2">
+          <label id="email">Email</label>
+          <input
+            className="p-2 px-3 placeholder:text-gray-500 bg-transparent border border-gray-400 text-[#C2E1E8] rounded-md"
+            type="email"
+            name="email"
+            placeholder="johndoe@gmail.com"
+            onChange={inputHandler}
+            value={signupCredentials.email}
+            ref={inputRef}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label id="email">Username</label>
+          <input
+            className="p-2 px-3 placeholder:text-gray-500 bg-transparent border border-gray-400 text-[#C2E1E8] rounded-md"
+            type="text"
+            name="username"
+            placeholder="johndoe779"
+            onChange={inputHandler}
+            value={signupCredentials.password}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label id="email">Password</label>
+          <input
+            className="p-2 px-3 placeholder:text-gray-500 bg-transparent border border-gray-400 text-[#C2E1E8] rounded-md"
+            type="password"
+            name="password"
+            placeholder="**********"
+            onChange={inputHandler}
+            value={signupCredentials.password}
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <button className="bg-brand shadow-sm p-2 font-bold rounded-md text-white">
+            SIGN UP
+          </button>
+          <span className="mt-2 text-gray-300">
+            Already had an Account?{" "}
+            <Link
+              href="/signup"
+              className="font-semibold text-white hover:underline hover:underline-offset-2"
+            >
+              Login
+            </Link>
+          </span>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignupForm;
