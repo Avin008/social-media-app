cd39260a2d69c657a2b9eb76abc0ba715e38bb33
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-04-05 22:32:09 +0530

    added: searchbar component

--- app/feeds/page.tsx
+++ app/feeds/page.tsx
@@ -1,14 +1,22 @@
+import SearchBar from "@/components/SearchBar";
 import Sidebar from "@/components/Sidebar";
 
 const UserFeedsPage = () => {
   return (
-    <main className="bg-background h-screen">
-      <div className="grid h-full grid-cols-12">
-        <div className="border border-gray-500 col-span-3">
+    <main className="bg-[#1E1F23] h-screen">
+      <div className="h-full w-[80%] bg-[#1E1F23] mx-auto grid grid-cols-12">
+        <div className="col-span-2 space-y-5">
+          <div className="m-5">
+            <span className="text-4xl text-brand font-extrabold">
+              Picco
+            </span>
+          </div>
           <Sidebar />
         </div>
-        <div className="border border-gray-500 col-span-6"></div>
-        <div className="border border-gray-500 col-span-3"></div>
+        <div className="col-span-6 border border-gray-600"></div>
+        <div className="border border-gray-600 col-span-4">
+          <SearchBar />
+        </div>
       </div>
     </main>
   );
---
+++ components/SearchBar.tsx
@@ -0,0 +1,16 @@
+const SearchBar = () => {
+  return (
+    <div className="relative shadow-sm bg-[#282C37] h-10 rounded-md m-2 flex items-center">
+      <input
+        className="text-white bg-transparent outline-none h-full w-[90%] px-4"
+        type="text"
+        placeholder="search peoples"
+      />
+      <div className="flex justify-end w-[10%] px-2">
+        <span>🔍</span>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
