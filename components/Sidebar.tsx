8e5c7652b95bc04b60d7b1ee37ea8149facf68ee
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-04-05 20:38:28 +0530

    style: updated styles

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,16 +1,16 @@
 const Sidebar = () => {
   return (
-    <ul className="h-fit text-white space-y-4 sticky top-14">
-      <li className="hover:bg-gray-500 bg-gray-500 p-3 cursor-pointer">
+    <ul className="pb-1 space-y-2 text-white cursor-pointer bg-[#1E1F23] flex flex-col">
+      <li className="hover:bg-brand rounded-l-md cursor-pointer transition-all p-3">
         🔥 Feeds
       </li>
-      <li className="hover:bg-gray-500 p-3 cursor-pointer">
+      <li className="hover:bg-brand rounded-l-md cursor-pointer transition-all p-3">
         🤔 Explore
       </li>
-      <li className="hover:bg-gray-500 p-3 cursor-pointer">
+      <li className="hover:bg-brand rounded-l-md cursor-pointer transition-all p-3">
         📺 Profile
       </li>
-      <li className="hover:bg-gray-500 p-3 cursor-pointer">
+      <li className="hover:bg-brand rounded-l-md cursor-pointer transition-all p-3">
         ☀️ Logout
       </li>
     </ul>
