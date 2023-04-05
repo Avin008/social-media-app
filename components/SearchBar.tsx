895754a03f8b7eab3c21154e95f1e9dd9339b237
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-04-05 22:36:10 +0530

    style: added search-bar focus style

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,6 @@
 const SearchBar = () => {
   return (
-    <div className="relative shadow-sm bg-[#282C37] h-10 rounded-md m-2 flex items-center">
+    <div className="relative shadow-sm focus-within:bg-transparent focus-within:border focus-within:border-brand bg-[#282C37] h-10 rounded-md m-2 flex items-center">
       <input
         className="text-white bg-transparent outline-none h-full w-[90%] px-4"
         type="text"
