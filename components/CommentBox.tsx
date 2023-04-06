33b282880e6fa1f46ac96cfb0f1144a315d5cfee
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-04-06 22:49:14 +0530

    added: post comment btn

--- components/CommentBox.tsx
+++ components/CommentBox.tsx
@@ -1,11 +1,12 @@
 const CommentBox = () => {
   return (
-    <div className="border w-full rounded-md h-full">
+    <div className="border flex w-full rounded-md h-full">
       <input
         className="w-full h-full rounded-md bg-transparent px-2"
         type="text"
         placeholder="comment here..."
       />
+      <button className="mx-2">💬</button>
     </div>
   );
 };
