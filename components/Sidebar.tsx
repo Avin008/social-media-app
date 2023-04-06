03bff7fbad8b0a372ae026cd977aeed0e9257fc4
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-04-06 12:06:42 +0530

    style: active link styles

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,16 +1,41 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <ul className="pb-1 space-y-2 text-white cursor-pointer bg-[#1E1F23] flex flex-col">
-      <li className="hover:bg-brand rounded-l-md cursor-pointer transition-all p-3">
+      <Link
+        className={`hover:bg-[#282C37] rounded-l-md cursor-pointer transition-all p-3 ${
+          pathname === "/feeds" && "bg-[#282C37]"
+        }`}
+        href="/feeds"
+      >
         🔥 Feeds
-      </li>
-      <li className="hover:bg-brand rounded-l-md cursor-pointer transition-all p-3">
+      </Link>
+
+      <Link
+        className={`hover:bg-[#282C37] rounded-l-md cursor-pointer transition-all p-3 ${
+          pathname === "/feeds/explore" && "bg-[#282C37]"
+        }`}
+        href="/feeds/explore"
+      >
         🤔 Explore
-      </li>
-      <li className="hover:bg-brand rounded-l-md cursor-pointer transition-all p-3">
+      </Link>
+
+      <Link
+        className={`hover:bg-[#282C37] rounded-l-md cursor-pointer transition-all p-3 ${
+          pathname === "/feeds/profile" && "bg-[#282C37]"
+        }`}
+        href="/feeds/profile"
+      >
         📺 Profile
-      </li>
-      <li className="hover:bg-brand rounded-l-md cursor-pointer transition-all p-3">
+      </Link>
+      <li
+        className={`hover:bg-[#282C37] rounded-l-md cursor-pointer transition-all p-3`}
+      >
         ☀️ Logout
       </li>
     </ul>
