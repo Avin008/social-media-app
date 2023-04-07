ff0a469a4d5b68886bb5bf9534ec18c2c3060e4f
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-04-08 04:05:52 +0530

    fix: post api link

--- components/CreatePostCard.tsx
+++ components/CreatePostCard.tsx
@@ -1,8 +1,51 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Avatar from "./Avatar";
+import { useMutation } from "react-query";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/store/useAuthStore";
 
 const CreatePostCard = () => {
+  const [post, setPost] = useState<{ postText: string }>({
+    postText: "",
+  });
+
+  const inputHandler = (
+    e: React.SyntheticEvent<HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.currentTarget;
+    setPost((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const { token } = useAuthStore((store) => store);
+
+  const router = useRouter();
+
+  const { isLoading, data, mutate } = useMutation(
+    async () => {
+      return fetch("http://localhost:3080/post", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          token: token,
+          postText: post.postText,
+        }),
+      }).then((res) => res.json());
+    },
+    {
+      onSuccess: (data: { message: string; data: any }) => {
+        setPost({ postText: "" });
+        if (data.message === "post created successfully") {
+          toast.success(data.message);
+        }
+      },
+      onError: () => {
+        toast.error("something went wrong");
+      },
+    }
+  );
+
   return (
     <div className="h-fit p-2">
       <div className="flex items-center">
@@ -12,9 +55,11 @@ const CreatePostCard = () => {
         <div className="w-[90%] border rounded-md">
           <textarea
             className="w-full p-2 resize-none border-none outline-none bg-transparent"
-            name=""
+            name="postText"
             id=""
+            value={post.postText}
             placeholder="Write your post here..."
+            onChange={inputHandler}
           ></textarea>
         </div>
       </div>
@@ -26,7 +71,10 @@ const CreatePostCard = () => {
           id=""
           accept=".png,.jpg,.jpeg"
         />
-        <button className="text-white text-sm bg-brand px-4 font-medium shadow-md py-1 rounded-full">
+        <button
+          onClick={() => mutate()}
+          className="text-white text-sm bg-brand px-4 font-medium shadow-md py-1 rounded-full"
+        >
           post
         </button>
       </div>
