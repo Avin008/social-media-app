297e97f92e247d12b410326c48a6fcc9a7f18065
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-04-09 21:42:14 +0530

    added: key prop

--- components/SearchList.tsx
+++ components/SearchList.tsx
@@ -7,7 +7,12 @@ const SearchList = () => {
     <div className="absolute left-0 py-1 right-0 top-12 min-h-20 shadow-sm border rounded-md">
       <ul>
         {data?.map((user) => (
-          <li className="hover:bg-[#282C37] cursor-pointer">
+          <li
+            key={
+              Date.now() + Math.floor(Math.random() * 1000)
+            }
+            className="hover:bg-[#282C37] cursor-pointer"
+          >
             <SearchedUser user={user} />
           </li>
         ))}
