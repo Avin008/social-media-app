7558efbe9037def4f2ba82823140bad6f04cf3f8
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-04-16 01:58:03 +0530

    feat: update profile api call

--- components/UpdateProfileCard.tsx
+++ components/UpdateProfileCard.tsx
@@ -1,5 +1,9 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Avatar from "./Avatar";
+import { useMutation } from "react-query";
+import axios from "axios";
+import { useAuthStore } from "@/store/useAuthStore";
+import { ClipLoader } from "react-spinners";
 const UpdateUserProfileCard = ({
   userData,
   toggleProfileCardHandler,
@@ -9,6 +13,11 @@ const UpdateUserProfileCard = ({
 }) => {
   const [updateUserData, setUpdateUserData] =
     useState<typeof userData>(userData);
+
+  const token = useAuthStore((store) => store.token);
+
+  const [updateProfilePic, setUpdateProfilePic] =
+    useState<FileList | null>();
 
   const inputHandler = (
     e: React.SyntheticEvent<HTMLInputElement>
@@ -21,6 +30,26 @@ const UpdateUserProfileCard = ({
     }));
   };
 
+  const imgUploadRef = useRef<HTMLInputElement>(null);
+
+  const { mutate, isLoading } = useMutation(async () => {
+    const formData = new FormData();
+    formData.append("fullname", updateUserData.fullname);
+    formData.append("username", updateUserData.username);
+    formData.append("email", updateUserData.email);
+    formData.append("password", updateUserData.password);
+    // @ts-ignore
+    formData.append("image", updateProfilePic[0]);
+    // @ts-ignore
+    formData.append("token", token);
+
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_URL}/user/update`,
+      formData
+    );
+    return res.data;
+  });
+
   return (
     <div className="p-5 text-white text-sm shadow-md flex flex-col items-center gap-5 py-5 bg-[#1E1F23] border-white h-fit w-96 rounded-md">
       <Avatar
@@ -28,7 +57,12 @@ const UpdateUserProfileCard = ({
         height={"60px"}
         image={updateUserData.profilePic}
       />
-      <button className="text-brand font-medium">
+      <button
+        className="text-brand font-medium"
+        onClick={() => {
+          imgUploadRef.current?.click();
+        }}
+      >
         change profile pic
       </button>
       <div className="flex flex-col w-full gap-2">
@@ -62,6 +96,12 @@ const UpdateUserProfileCard = ({
           className="bg-transparent hidden rounded-md px-3 border p-2 w-full"
           placeholder="johndoe776"
           type="file"
+          ref={imgUploadRef}
+          onChange={(
+            e: React.ChangeEvent<HTMLInputElement>
+          ) => {
+            setUpdateProfilePic(e.currentTarget.files);
+          }}
         />
       </div>
       <div className="flex w-full flex-col gap-2">
@@ -91,8 +131,17 @@ const UpdateUserProfileCard = ({
         />
       </div>
       <div className="flex flex-col w-full gap-2">
-        <button className="w-full p-2 rounded-md bg-brand text-white">
-          Update Profile
+        <button
+          className="w-full p-2 flex items-center justify-center rounded-md bg-brand text-white"
+          onClick={() => {
+            mutate();
+          }}
+        >
+          {!isLoading ? (
+            "Update Profile"
+          ) : (
+            <ClipLoader size={20} color="white" />
+          )}
         </button>
         <button
           className="w-full p-2 rounded-md border border-brand text-brand font-semibold"
