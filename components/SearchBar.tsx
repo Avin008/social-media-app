b4512458ea73e73f405c5053135a41cc78fbeeeb
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-04-15 23:07:43 +0530

    feat: added seach input handler

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,24 @@
+"use client";
+import { useState } from "react";
 import SearchList from "./SearchList";
 
 const SearchBar = () => {
+  const [searchKey, setSearchKey] = useState<string>("");
+
+  const searchInputHandler = (
+    e: React.SyntheticEvent<HTMLInputElement>
+  ) => {
+    setSearchKey(e.currentTarget.value);
+  };
+
   return (
     <div className="relative shadow-sm focus-within:bg-transparent focus-within:border focus-within:border-brand bg-[#282C37] h-10 rounded-md m-2 flex items-center">
       <input
         className="text-white bg-transparent outline-none h-full w-[90%] px-4"
         type="text"
         placeholder="search peoples"
+        onChange={searchInputHandler}
+        value={searchKey}
       />
       <div className="flex justify-end w-[10%] px-2">
         <span>🔍</span>
