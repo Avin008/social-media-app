80d913b2ee9dc17f8348ccdb2eda6856be0b2e34
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-04-16 18:48:24 +0530

    fix: incorrent type defination

--- components/UserComment.tsx
+++ components/UserComment.tsx
@@ -3,18 +3,18 @@ import Avatar from "./Avatar";
 const UserComment = ({
   comment,
 }: {
-  comment: Comments;
+  comment: CommentType;
 }) => {
   return (
     <div className="items-center flex gap-1">
       <div className="w-[10%]">
-        <Avatar image={comment.author.profilePic} />
+        <Avatar image={comment?.author?.profilePic} />
       </div>
       <div className="w-[90%] flex flex-col leading-4">
         <span className="text-xs text-gray-400">
-          @{comment.author.fullname}
+          @{comment?.author?.fullname}
         </span>
-        <span className="text-xs">{comment.comment}</span>
+        <span className="text-xs">{comment?.comment}</span>
       </div>
     </div>
   );
